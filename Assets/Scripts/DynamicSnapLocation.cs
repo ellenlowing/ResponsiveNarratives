99d4f6c79215d06659330df0ab64e05bb90118a9
Ellen Lo <lowing@bu.edu>
2024-01-14 02:33:22 -0800

    Snap to raycast surface

---
+++ Assets/Scripts/DynamicSnapLocation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicSnapLocation : MonoBehaviour
+{
+    [SerializeField]private Transform _pinchTransform;
+    [SerializeField] private OVRHand _rightHand;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        UpdateSnapLocation();
+    }
+
+    public void UpdateSnapLocation()
+    {
+        RaycastHit hit;
+        int layerMask = 1 << 3;
+        if(_rightHand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+        {
+            if(Physics.Raycast(_pinchTransform.position, _pinchTransform.forward, out hit, Mathf.Infinity, layerMask ))
+            {
+                transform.position = hit.point;
+                transform.rotation = Quaternion.LookRotation(-hit.normal, Vector3.up);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PassthroughPlaneHandler.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(OVRSceneAnchor))]
+public class PassthroughPlaneHandler : MonoBehaviour
+{
+    private OVRSceneAnchor _sceneAnchor;
+    private OVRSemanticClassification _classification;
+
+    void Start()
+    {
+        _sceneAnchor = GetComponent<OVRSceneAnchor>();
+        _classification = GetComponent<OVRSemanticClassification>();
+        ClassifyAndTag();
+    }
+
+    void ClassifyAndTag()
+    {
+        var plane = _sceneAnchor.GetComponent<OVRScenePlane>();
+        var dimensions = Vector3.one;
+
+        if(_classification && plane)
+        {
+            dimensions = plane.Dimensions;
+            dimensions.z = 1;
+            gameObject.layer = LayerMask.NameToLayer("PassthroughPlane");
+            gameObject.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("PassthroughPlane");
+
+            // if(_classification.Contains(OVRSceneManager.Classification.Floor))
+            // {
+            //     gameObject.layer = LayerMask.NameToLayer("Floor");
+            //     gameObject.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("Floor");
+            //     gameObject.tag = "Floor";
+            //     gameObject.transform.GetChild(0).tag = "Floor";
+            // }
+            // else if (_classification.Contains(OVRSceneManager.Classification.Ceiling))
+            // {
+            // }
+            // else if (_classification.Contains(OVRSceneManager.Classification.WallFace))
+            // {
+            //     gameObject.layer = LayerMask.NameToLayer("Wall");
+            //     gameObject.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("Wall");
+            //     gameObject.tag = "Wall";
+            //     gameObject.transform.GetChild(0).tag = "Wall";
+
+            // }
+        }
+    }
+}
